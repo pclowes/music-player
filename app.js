77c4401db7d263eb8ec73cc1d8f42d5804078009
pclowes <peter.d.clowes@gmail.com>
2015-01-20 10:53:58 -0700

    fixed pause vs stop issue

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ $('.tracks').on('click', '.fa-play', function() {
   $(".fa").addClass('fa-play').removeClass('fa-stop');
   $(this).removeClass('fa-play').addClass('fa-stop');
   var playing = $(this).data('title');
-  $('h2.select').html("Now Playing: " + playing);
+  $('h2.select').html("Now Playing:  " + playing);
+  $('audio').each(function () {
+    this.pause();
+    this.currentTime = 0;
+  });
   var id = $(this).data('id');
   document.getElementById(id).play();
+
+
 });
 
 $('.tracks').on('click', '.fa-stop', function() {
   $(this).addClass('fa-play').removeClass('fa-stop');
   $('h2.select').html("Select a Song!");
   var id = $(this).data('id');
-  document.getElementById(id).play();
+  document.getElementById(id).pause();
 });
 
 $.getJSON('data.json', function(tracks) {
